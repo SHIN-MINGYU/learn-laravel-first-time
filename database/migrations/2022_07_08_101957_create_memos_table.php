ceb37ec76d0c9f5b5fcc573f6d3fe91250f45a63
SHIN_MINGYU <smg20004@naver.com>
2022-07-08 19:56:44 +0900

    learn database migration

---
+++ database/migrations/2022_07_08_101957_create_memos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMemosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('memos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->longText('content');
+            $table->integer('user_id');
+            $table->integer('status')->default('1');
+            //timestampと書いてしまうと、レコード挿入時、変更時に値が入らないのでDB::RAWで直接書く
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamp('create_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('memos');
+    }
+}
--- server.php
+++ server.php
@@ -6,6 +6,7 @@
  * @package  Laravel
  * @author   Taylor Otwell <taylor@laravel.com>
  */
+//start with 'php artisan serve' command
 
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? ''
@@ -19,3 +20,4 @@ if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
 }
 
 require_once __DIR__.'/public/index.php';
+
